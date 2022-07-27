63f97b4277509b21f9881365ae4464e49530108e
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-07-27 12:32:29 -0300

    Adicionando rota para nova tela

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, "index"])->name("home");
+Route::get("/deputado/{id}", [\App\Http\Controllers\HomeController::class, "show"])->name("deputado.detalhes")->where("id", "\d+");
