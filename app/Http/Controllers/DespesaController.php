f96638dc7d8a19fe7cdaf45a94ab5c3346e8b285
diegomatos321 <diegovsm@dcc.ufrj.br>
2022-07-30 16:00:36 -0300

    Fix soma das despesas: Iterando por todas a paginas e somando as despesas

--- app/Http/Controllers/DespesaController.php
+++ app/Http/Controllers/DespesaController.php
@@ -36,8 +36,10 @@ class DespesaController extends Controller
 
     public function show(Request $request, int $id) {
         try {
+            $page = $request->query("pagina", 1);
+
             $deputado = (new DeputadoController())->GetDeputadoFromAPI($id)->dados;
-            $despesas = $this->GetDespesaPorDeputado($id)->dados;
+            $despesas = $this->GetDespesaPorDeputado($id, $page)->dados;
 
             return view("deputado.despesas", [
                 "deputado" => $deputado,
@@ -55,11 +57,35 @@ class DespesaController extends Controller
         if (Cache::has($cacheKey)) {
            $result = Cache::get($cacheKey);
         } else {
-            $listOfCosts = $this->GetDespesaPorDeputado($deputado->id)->dados;
-            $sumfOfCosts = 0;
+            $initialPage = 1;
+            $lastLinkQueryArr = [];
 
-            foreach ($listOfCosts as $key => $cost) {
+            $tmpCost = $this->GetDespesaPorDeputado($deputado->id, $initialPage);
+
+            $lastLink = end($tmpCost->links);
+            $lastLinkQueryString = parse_url($lastLink->href, PHP_URL_QUERY);
+            
+            if ($lastLinkQueryString == false) {
+                throw "Erro ao interpretar URL da última página";
+            }
+            
+            parse_str($lastLinkQueryString, $lastLinkQueryArr);
+            $lastPage = $lastLinkQueryArr["pagina"];
+
+            $sumfOfCosts = 0;
+            foreach ($tmpCost->dados as $key => $cost) {
                 $sumfOfCosts += $cost->valorLiquido;
+            }
+            $initialPage++;
+
+            while($initialPage != $lastPage) {
+                $listOfCosts = $this->GetDespesaPorDeputado($deputado->id, $initialPage)->dados;
+
+                foreach ($listOfCosts as $key => $cost) {
+                    $sumfOfCosts += $cost->valorLiquido;
+                }
+
+                $initialPage++;
             }
 
             $result = json_encode([
@@ -75,25 +101,30 @@ class DespesaController extends Controller
         return json_decode($result);
     }
 
-    public function GetDespesaPorDeputado(int $id): object {
-        $cacheKey = "despesas-deputado-" . $id;
-        $endpoint = env("DADOS_ABERTOS_API_URL") . "/deputados/" . $id . "/despesas";
+    public function GetDespesaPorDeputado(int $id, int $page): object {
+        $cacheKey = "despesas-deputado-" . $id . "-pagina-" . $page;
         $result = null;
 
         if (Cache::has($cacheKey)) {
             $result = Cache::get($cacheKey);
         } else {
+            $endpoint = env("DADOS_ABERTOS_API_URL") . "/deputados/" . $id . "/despesas";
             $response = null;
+            $query = [
+                "ordem" => "DESC",
+                "ordenarPor" => "dataDocumento",
+                "pagina" => $page
+            ];
 
             if (env("APP_ENV") == "local") {
                 $response = Http::withOptions([
                         "verify" => false
                     ])
                     ->accept("application/json")
-                    ->get($endpoint);
+                    ->get($endpoint, $query);
             } else {
                 $response = Http::accept("application/json")
-                ->get($endpoint);
+                ->get($endpoint, $query);
             }
             
             if ($response->failed()) {
